c1e187f8f397d735dfd57ac101c49cb26ad45112
vuongbachdoan <vuongbachdoan@gmail.com>
2022-07-30 11:42:34 +0700

    add image upload function + preview image

--- src/NFT_prepare_frontend/src/components/spec/Create.jsx
+++ src/NFT_prepare_frontend/src/components/spec/Create.jsx
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Create() {
+  const [imgUri, setImgUri] = useState("");
+
+  const getFile = (e) => {
+    let file = e.target.files[0];
+
+    if(file){
+      const reader = new FileReader();
+      reader.onload = () => {
+        let result = reader.result;
+        setImgUri(result)
+      }
+      reader.readAsDataURL(file);
+    }
+  }
+
+  const generateNft = () => {
+    
+  }
+
   return (
-    <div>Create</div>
+    <div>
+      <div className="input-group mb-3">
+        <input type="file" className="form-control" id="inputGroupFile02" accept=".jpeg,.jpg,.png,.gif,image" onChange={(e) => getFile(e)}/>
+        <label className="input-group-text" htmlFor="inputGroupFile02" onClick={() => generateNft}>Upload</label>
+      </div>
+      <img src={imgUri} alt="preview"/>
+    </div>
   )
 }
